abd18b90644d6b00d569b86e8d553482fbe4dcde
Zeeshan Syed <zsyed89@gmail.com>
2019-06-15 17:38:04 -0400

    Init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/landing_page.dart';
+
+import 'services/auth.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: LandingPage(
+        auth: Auth(),
+      ),
+    );
+  }
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+class User {
+  User({@required this.uid});
+  final String uid;
+}
+
+abstract class AuthBase {
+  Future<User> currentUser();
+  Future<User> signInAnonymously();
+  Future<void> signOut();
+}
+
+class Auth implements AuthBase {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User _userFromFirebase(FirebaseUser user) {
+    if (user == null) {
+      return null;
+    }
+    return User(uid: user.uid);
+  }
+
+  Future<User> currentUser() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    return _userFromFirebase(user);
+  }
+
+  Future<User> signInAnonymously() async {
+    FirebaseUser user = await FirebaseAuth.instance.signInAnonymously();
+    return _userFromFirebase(user);
+  }
+
+  Future<void> signOut() async {
+    return await _firebaseAuth.signOut();
+  }
+}
